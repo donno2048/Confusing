d78a9a3e1ecf583982b7ed76dfa89cdb17a15a28
Elisha Hollander <just4now666666@gmail.com>
2021-02-02 01:14:35 +0200

    Bug fixes

--- Con1.java
+++ Con1.java
@@ -1,10 +1,10 @@
 import java.lang.reflect.Field; import java.util.
 Random;public class Con1 {public static int j=100
-;public static void main(String[]args){if(args[0]
-.length()>0)j=Math.min(Integer.parseInt(args[0])+
-1,j);for(int i=1;i<j;i++)System.out.printf("%d",i
-);}public static double _one() { return 0.1 + 0.1
-+ 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;}
+;public static void main(String[] args) {if(args.
+length>0&&args[0].length()>0)j=Math.min (Integer.
+parseInt(args[0])+1,j);for(int i=1;i<j;i++)System
+.out.printf("%d",i);} public static double _one()
+{return 0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1;}
 ;public static void error(int ImpossibleNumber‮ ) {
 System.err.println ("\n" + ImpossibleNumber + " "
 + "Is impossible, maybe try other value for j");}
