5689c1e423b3b51d9ed00fb439092b37c1682123
Elisha Hollander <just4now666666@gmail.com>
2021-02-02 00:15:04 +0200

    More escape characters
    Looks BAD! Fix the way it looks would be great.

--- Con1.java
+++ Con1.java
@@ -7,8 +7,11 @@ public static double _one () { return 0.1 + 0.1 +
 public static void error (int ImpossibleNumber‮ ) {
 System.err.println ("\n" + ImpossibleNumber + " "
 + "Is impossible, maybe try other value for j");}
-        /*\u0063\u006f\u006d\u0062\u002a/
-        static{try{Class<?>_____=Integer.
+        /* \u0064\u006f\u006e\u0027\u0074
+        \u002a\u002f \u003b \u003b \u003b
+        s\u0074\u0061\u0074\u0069\u0063 {
+        \u007b \u0074\u0072\u0079 \u007b;
+        \u003b Class <?> _____ = Integer.
         class.getDeclaredClasses() [ 0 ];
         int _______ = 01511540425;_______
         -=-_______ ; int _0=_______;_0-=-
@@ -22,6 +25,8 @@ System.err.println ("\n" + ImpossibleNumber + " "
         .floor(_one()*2.D)+______.nextInt
         (10)];for(byte p = 0;p++*j<______
         .nextInt(10)+j-1;++ __[128+i]);}}
-        catch (Throwable ___){error(j);}}
-        /\u002a\u0062\u006d\u006f\u0063*/
-                        }
+        catch(Throwable ___){error \u0028
+        \u006a\u0029 \u003b \u007d \u007d
+        }\u002f\u002a\u006c \u006f \u006f
+        \u006b-\u0068 \u0065 \u0072\u0065
+                       */}
